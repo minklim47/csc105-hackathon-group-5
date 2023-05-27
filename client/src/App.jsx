5eae63876d3f91614e95552a060e9baeb9c7bd31
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-05-27 15:38:54 +0700

    style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,12 @@ import {
 } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import ParticleBackground  from "./particles/ParticleBackground";
 
 function App() {
   return (
     <BrowserRouter>
+    {/* <ParticleBackground/> */}
       <Routes>
         <Route exact path="/home" element={<Home />} />
         {/* <Route path="profile" element={}/> */}

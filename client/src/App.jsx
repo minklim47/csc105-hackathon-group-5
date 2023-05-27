60633ca58cc7b60a09a61b4d68476ff81a290729
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-05-27 13:49:06 +0700

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,13 @@ import {
   Navigate,
 } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route exact path="/" element={<Home />} />
         <Route />
         <Route />
         <Route />

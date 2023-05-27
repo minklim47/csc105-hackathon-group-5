b360fba7f7373e93b63c2b19f7731101b3bd854f
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-05-27 10:01:23 +0700

    set up frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useParams,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

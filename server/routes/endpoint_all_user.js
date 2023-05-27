b8425176a83e1c96b2afe417bb0ed558264ed7ee
Watsawadee <109060632+Watsawadee@users.noreply.github.com>
2023-05-28 00:36:48 +0700

    update specific star and user

--- server/routes/endpoint_all_user.js
+++ server/routes/endpoint_all_user.js
@@ -4,6 +4,26 @@ const mysql = require("mysql2");
 const router = express.Router();
 
 module.exports = (connection) => {
+    router.get("/:userId", (req, res) => {
+        const userId = req.params.userId;
+        const sqlSelect = "SELECT * FROM group05.users WHERE id = ?";
+        connection.query(sqlSelect, [userId], (err, rows) => {
+          if (err) {
+            res.json({
+              success: false,
+              data: null,
+              error: err.message,
+            });
+          } else {
+            // Return data to the client if successful
+            res.json({
+              success: true,
+              data: rows,
+              error: null,
+            });
+          }
+        });
+      });
     router.get("/", (req, res) => {
         const sqlSelect = "SELECT * FROM group05.users";
 

394f3c09c050d701b1f2f0838e7c368a2781ea30
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-05-27 19:16:31 +0700

    moon

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import Profile from '../components/profile'
+import Star from '../components/Star'
+import moon from '../assets/moon.png'
 
 function Home() {
   return (
     <div>Home
-        
-
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+        <img style={moonStyle} src={moon}/>
     </div>
   )
 }
 
+
 export default Home
+const moonStyle ={ 
+    position:"fixed",
+    maxWidth:"800px",
+    width:"70%",
+    right:"-200px",
+    bottom:"-300px",
+    
+    zIndex:"10",
+}

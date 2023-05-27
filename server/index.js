a0da19c07721aa770b4337641e76ec6d2e83b4ba
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-05-27 10:22:36 +0700

    set up

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const connection = mysql.createConnection({
+    host: "db.cshack.site",
+    port: "3306",
+    user: "ggroup05",
+    password: "205223224",
+    database: "Hackathon_G5",
+});
+
+connection.connect((err) => {
+    if (!err) {
+        console.log("\nDatabase connected\n");
+    } else {
+        console.error("Error connecting to database", err);
+    }
+});
+
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!!!");
+});
+
+const loginRoute = require('./routes/endpoint_login')(connection);
+const signupRoute = require('./routes/endpoint_signup')(connection);
+
+app.use('/login', loginRoute);
+app.use('/signup', loginRoute);
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+});

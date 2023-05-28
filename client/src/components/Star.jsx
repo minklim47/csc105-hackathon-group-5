1c8c1289ea5148fdc9f335cde06596985fab568a
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-05-28 10:21:50 +0700

    sad

--- client/src/components/Star.jsx
+++ client/src/components/Star.jsx
@@ -2,21 +2,8 @@ import React, { useState, useEffect } from "react";
 import "../styles/star.css"; // Import the CSS file for styling
 import starImg from "../assets/star.png";
 
-// const instance = Axios.create({
-//   withCredentials: true,
-// });
-const Star = ({ star }) => {
+const Star = () => {
   
-
-  const handleClick = () => {
-    // instance
-    //   .get("http://localhost:8000/allstar")
-    //   .then((res) => {
-    //     console.log(res.data);
-    //     set
-    //   })
-    //   .catch((err) => {console.log(err)});
-  };
   return (
     <div className="star"  >
       <img src={starImg} style={{ width: "40px" }} />
@@ -26,6 +13,3 @@ const Star = ({ star }) => {
 
 export default Star;
 
-// const starStyle = {
-//   width:"40px"
-// }
